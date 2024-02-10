760f29cbe301960e58f8430922e8edd3ad69c99c
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-10 19:03:01 +0000

    a

--- Project/app.js
+++ Project/app.js
@@ -100,7 +100,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error");
   // res.send("Oh boy, something went wrong");
 });
 app.listen(3000, () => {
