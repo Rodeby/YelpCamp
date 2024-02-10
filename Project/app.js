f9cf2b217feab5b422889699a5e33d92ad8c6f78
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-10 19:27:53 +0000

    a

--- Project/app.js
+++ Project/app.js
@@ -57,6 +57,8 @@ app.post(
       campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
       }).required(),
     });
 
