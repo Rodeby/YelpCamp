3b2bf378a07046c49588e044a2cbc310bf10b071
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-10 18:49:31 +0000

    a

--- Project/app.js
+++ Project/app.js
@@ -5,6 +5,7 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
