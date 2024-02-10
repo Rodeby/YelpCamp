ca4f088303899d76e85825f5f19b1307d0ca9df9
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-10 18:35:13 +0000

    a

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,3 +1,7 @@
 class ExpressError extends Error {
-  constructor(message, statusCode) {}
+  constructor(message, statusCode) {
+    super();
+    this.message = message;
+    this.status = statusCode;
+  }
 }
