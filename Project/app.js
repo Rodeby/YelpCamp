2c167349dae00fb35e54880c942b09398efe9b65
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-11 17:43:35 +0000

    a

--- Project/app.js
+++ Project/app.js
@@ -113,6 +113,8 @@ app.post(
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
+    await review.save();
+    await campground.save();
   })
 );
 app.all("*", (req, res, next) => {
