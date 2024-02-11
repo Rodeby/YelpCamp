6d642217197fdbf204f26dd3a7d8646bcd3c967a
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-11 23:45:05 +0000

    a

--- Project/routes/campgrounds.js
+++ Project/routes/campgrounds.js
@@ -6,6 +6,15 @@ const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review.js");
 
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 router.get(
   "/",
   catchAsync(async (req, res) => {
