8221385515ac141036bbbc627e7176b9225d24c2
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-11 23:42:11 +0000

    a

--- Project/routes/campgrounds.js
+++ Project/routes/campgrounds.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { model } = require("mongoose");
 const router = express.Router();
+const catchAsync = require("./utils/catchAsync");
 
 router.get(
   "/",
