0570f30e1f9b65e6ab5da6b3a79d0cb2595abbd7
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-12 00:04:14 +0000

    a

--- Project/routes/reviews.js
+++ Project/routes/reviews.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review.js");
 const { campgroundSchema, reviewSchema } = require("../schemas.js");
-const router = express.Router({ mergeParams: true });
 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
