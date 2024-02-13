f11253ae32f6e55f26a89381fef2d7b5728e8ad2
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-13 19:52:27 +0000

    a

--- Project/routes/campgrounds.js
+++ Project/routes/campgrounds.js
@@ -81,9 +81,9 @@ router.put(
       res.flash("error", "You don't have permission to do that");
       return res.redirect(`/campgrounds/${id}`);
     }
-    // const campground = await Campground.findByIdAndUpdate(id, {
-    //   ...req.body.campground,
-    // });
+    const camp = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
     req.flash("success", "Successfully updated campground!");
     res.redirect(`/campgrounds/${campground._id}`);
   })
