1a6298ecb6d7da7fce14a6d82c741fdcf73fd90e
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-02-14 22:58:27 +0000

    a

--- Project/routes/users.js
+++ Project/routes/users.js
@@ -7,24 +7,7 @@ const users = require("../controllers/users");
 
 router.get("/register", users.renderRegister);
 
-router.post(
-  "/register",
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash("success", "Welcome to Yelp Camp!");
-        res.redirect("/campgrounds");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("register");
-    }
-  })
-);
+router.post("/register", catchAsync(users.register));
 
 router.get("/login", (req, res) => {
   res.render("users/login");
