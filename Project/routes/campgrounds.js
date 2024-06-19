b66e2565f1fb4d687a68ef298c8a5ad03f47d7fe
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-06-19 06:32:08 +0100

    a

--- Project/routes/campgrounds.js
+++ Project/routes/campgrounds.js
@@ -4,7 +4,9 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const multer = require("multer");
-const upload = multer();
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
+
 const Campground = require("../models/campground");
 
 router
@@ -12,6 +14,7 @@ router
   .get(catchAsync(campgrounds.index))
   .post(
     isLoggedIn,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.createCampground)
   );
@@ -24,6 +27,7 @@ router
   .put(
     isLoggedIn,
     isAuthor,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.updateCampground)
   )
