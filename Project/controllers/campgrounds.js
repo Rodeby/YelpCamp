7677cf22d30089096c161ee3392dfc8a1ee35304
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-06-24 22:12:49 +0100

    a

--- Project/controllers/campgrounds.js
+++ Project/controllers/campgrounds.js
@@ -14,10 +14,12 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
-  const response: await geocoder.forwardGeocode({
-    query: 'Yosemite, CA',
-    limit: 1
-  }).send();
+  const geoData = geocoder
+    .forwardGeocode({
+      query: "Yosemite, CA",
+      limit: 1,
+    })
+    .send();
   const campground = new Campground(req.body.campground);
   //create array with only the relevant information. e.g. original is fieldname: 'image',
   campground.images = req.files.map((f) => ({
