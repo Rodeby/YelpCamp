9d85f72cf45efee3aa9559536fecf6bfddde58f4
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-06-25 20:20:17 +0100

    a

--- Project/public/javascripts/showPageMap.js
+++ Project/public/javascripts/showPageMap.js
@@ -3,5 +3,5 @@ const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
   center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+  zoom: 10, // starting zoom
 });
