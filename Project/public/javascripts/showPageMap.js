2d1b6c6f7a626a0f520f5de4871a31132fb8bc1d
Charlie Christiansson <55799475+Rodeby@users.noreply.github.com>
2024-06-25 20:35:13 +0100

    a

--- Project/public/javascripts/showPageMap.js
+++ Project/public/javascripts/showPageMap.js
@@ -1,8 +1,10 @@
+const campground = require("../../models/campground");
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: campground.geomertry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 
